5d7961925593b7184ad5ffdb805345d5323b9f34
tomohiko sato <tomohikosato1@gmail.com>
2016-12-23 20:31:13 +0900

    とりあえずTrash

--- app/src/main/java/com/example/tomohiko_sato/mywindow/TrashView.java
+++ app/src/main/java/com/example/tomohiko_sato/mywindow/TrashView.java
@@ -2,30 +2,40 @@ package com.example.tomohiko_sato.mywindow;
 
 import android.content.Context;
 import android.graphics.PixelFormat;
+import android.os.Build;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class TrashView extends ImageView {
     private final WindowManager wm;
     private final WindowManager.LayoutParams wmParams;
+    private final DisplayMetrics metrics;
 
     public TrashView(Context context) {
         super(context);
         wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        metrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(metrics);
+
         wmParams = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
+                96,
+                96,
                 WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,       // アプリケーションのTOPに配置
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |  // フォーカスを当てない(下の画面の操作ができなくなるため)
                         WindowManager.LayoutParams.FLAG_FULLSCREEN,        // OverlapするViewを全画面表示
                 PixelFormat.TRANSLUCENT);  // viewを透明にする
         wmParams.gravity = Gravity.BOTTOM;
+
+        setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         setImageResource(R.drawable.trash);
         wm.addView(this, wmParams);
     }
 
-    public void show () {
+    public void show() {
 
     }
 
