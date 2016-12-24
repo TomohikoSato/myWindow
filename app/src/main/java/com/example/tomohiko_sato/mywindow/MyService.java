da4100ab34d3b540ba2ffa5fa8fc8197d4559562
tomohiko sato <tomohikosato1@gmail.com>
2016-12-24 20:53:34 +0900

    とちゅー

--- app/src/main/java/com/example/tomohiko_sato/mywindow/MyService.java
+++ app/src/main/java/com/example/tomohiko_sato/mywindow/MyService.java
@@ -4,12 +4,14 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Rect;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubePlayerView;
@@ -32,27 +34,63 @@ public class MyService extends Service {
         super.onCreate();
         windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
         draggableView = new DraggableView(this);
-        /*draggableView.setOnMoveListener(new DraggableView.OnMoveListener() {
-            @Override
-            public void onMove(float x, float y) {
-                trashView.isIntercect(x, y)
-            }
-        });*/
         draggableView.setOnTouchListener(new View.OnTouchListener() {
+            Rect draggableRect = new Rect();
+            Rect trashRect = new Rect();
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_UP) {
-                    if (trashView.contains(event.getRawX(), event.getRawY())) {
+/*
+                    draggableView.getGlobalVisibleRect(draggableRect);
+                    trashView.getGlobalVisibleRect(trashRect);
+*/
+                    draggableRect = getRectInScreen(draggableView);
+                    trashRect = getRectInScreen(trashView);
+
+                    if (Rect.intersects(draggableRect, trashRect)) {
                         windowManager.removeView(draggableView);
                     }
                 }
                 return false;
             }
-        });
 
+            private Rect getRectInScreen(View view) {
+                final int[] locationOnScreen = new int[2];
+                view.getLocationOnScreen(locationOnScreen);
+                Log.d(TAG, "location on screen : " + locationOnScreen[0] + ", " + locationOnScreen[1]);
+                int left = locationOnScreen[0];
+                int top = locationOnScreen[1];
+                Rect rect = new Rect(left, top, left + view.getWidth(), top + view.getHeight());
+                Log.d(TAG, "rect: " + rect);
+                return rect;
+            }
+
+            private void logView(View view) {
+                Log.d(TAG, "left: " + view.getLeft() + " right: " + view.getRight() + " top: " + view.getTop() + " bottom: " + view.getBottom() + " paddingleft: " + view.getPaddingLeft() + " paddingTop: " + view.getPaddingTop() + " paddingRight: " + view.getPaddingRight() + " paddingBottom: " + view.getPaddingBottom());
+                Log.d(TAG, "width: " + view.getWidth() + " height: " + view.getHeight() + " x: " + view.getX() + " y: " + view.getY());
+                Rect drawingRect = new Rect();
+                view.getDrawingRect(drawingRect);
+                Log.d(TAG, "getDrawingRect: " + drawingRect);
+
+
+                final int[] locationOnScreen = new int[2];
+                view.getLocationOnScreen(locationOnScreen);
+                Log.d(TAG, "location on screen : " + locationOnScreen[0] + ", " + locationOnScreen[1]);
+
+                final int[] locationOnWindow = new int[2];
+                view.getLocationInWindow(locationOnWindow);
+                Log.d(TAG, "location on window : " + locationOnWindow[0] + ", " + locationOnWindow[1]);
+
+                Rect rectInGlobal = new Rect();
+                view.getGlobalVisibleRect(rectInGlobal);
+                Log.d(TAG, "globalVisibleRect=" + rectInGlobal);
+            }
+        });
 
         trashView = new TrashView(this);
     }
+
 
     public static void startService(Context context) {
         Intent intent = new Intent(context, MyService.class);
